6cb896d34149895f33711a3489951a5f3b9943b1
weiyang-1 <34814137+weiyang-1@users.noreply.github.com>
2019-04-01 11:40:12 +0800

    v=1.0.4

--- confs/conf.py
+++ confs/conf.py
@@ -28,7 +28,7 @@ AliYun_jobs = {
     "189":{
         "ip":"192.168.11.189",
         "user_name":"liuchao",
-        "pwd":"jianxun1302",
+        "pwd":"xxxxxxx1302",
         "jobs":[
             {
                 "path": "/home/liuchao",
@@ -47,105 +47,105 @@ AliYun_jobs = {
     },
     "201": {
         "ip": "192.168.11.201",
-        "user_name": "jianxun",
+        "user_name": "xxxxxxx",
         "pwd": "123!@#",
         "jobs": [
             {
-                "path": "/home/jianxun",
+                "path": "/home/xxxxxxx",
                 "type": "stop_zhipin.sh"
             },
             {
-                "path": "/home/jianxun",
+                "path": "/home/xxxxxxx",
                 "type": "restart_zhipin.sh"
             }
         ]
     },
     "202": {
         "ip": "192.168.11.202",
-        "user_name": "jianxun",
+        "user_name": "xxxxxxx",
         "pwd": "123!@#",
         "jobs": [
             {
-                "path": "/home/jianxun",
+                "path": "/home/xxxxxxx",
                 "type": "stop_zhipin.sh"
             },
             {
-                "path": "/home/jianxun",
+                "path": "/home/xxxxxxx",
                 "type": "restart_zhipin.sh"
             }
         ]
     },
     "203": {
         "ip": "192.168.11.203",
-        "user_name": "jianxun",
+        "user_name": "xxxxxxx",
         "pwd": "123!@#",
         "jobs": [
             {
-                "path": "/home/jianxun",
+                "path": "/home/xxxxxxx",
                 "type": "stop_zhipin.sh"
             },
             {
-                "path": "/home/jianxun",
+                "path": "/home/xxxxxxx",
                 "type": "restart_zhipin.sh"
             }
         ]
     },
     "205": {
         "ip": "192.168.11.205",
-        "user_name": "jianxun",
+        "user_name": "xxxxxxx",
         "pwd": "123!@#",
         "jobs": [
             {
-                "path": "/home/jianxun",
+                "path": "/home/xxxxxxx",
                 "type": "stop_zhipin.sh"
             },
             {
-                "path": "/home/jianxun",
+                "path": "/home/xxxxxxx",
                 "type": "restart_zhipin.sh"
             }
         ]
     },
     "206": {
         "ip": "192.168.11.206",
-        "user_name": "jianxun",
+        "user_name": "xxxxxxx",
         "pwd": "123!@#",
         "jobs": [
             {
-                "path": "/home/jianxun",
+                "path": "/home/xxxxxxx",
                 "type": "stop_zhipin.sh"
             },
             {
-                "path": "/home/jianxun",
+                "path": "/home/xxxxxxx",
                 "type": "restart_zhipin.sh"
             }
         ]
     },
     "244": {
         "ip": "192.168.11.244",
-        "user_name": "jianxun",
+        "user_name": "xxxxxxx",
         "pwd": "123!@#",
         "jobs": [
             {
-                "path": "/home/jianxun",
+                "path": "/home/xxxxxxx",
                 "type": "stop_zhipin.sh"
             },
             {
-                "path": "/home/jianxun",
+                "path": "/home/xxxxxxx",
                 "type": "restart_zhipin.sh"
             }
         ]
     },
     "245": {
         "ip": "192.168.11.245",
-        "user_name": "jianxun",
+        "user_name": "xxxxxxx",
         "pwd": "123!@#",
         "jobs": [
             {
-                "path": "/home/jianxun",
+                "path": "/home/xxxxxxx",
                 "type": "stop_zhipin.sh"
             },
             {
-                "path": "/home/jianxun",
+                "path": "/home/xxxxxxx",
                 "type": "restart_zhipin.sh"
             }
         ]
@@ -156,42 +156,42 @@ BaiDuYun_jobs = {
     "4": {
         "ip": "192.168.0.4",
         "user_name": "yangwei",
-        "pwd": "jianxun123"
+        "pwd": "xxxxxxx123"
     },
     "5": {
         "ip": "192.168.0.5",
         "user_name": "yangwei",
-        "pwd": "jianxun123"
+        "pwd": "xxxxxxx123"
     },
     "6": {
         "ip": "192.168.0.6",
         "user_name": "yangwei",
-        "pwd": "jianxun123"
+        "pwd": "xxxxxxx123"
     },
     "7": {
         "ip": "192.168.0.7",
         "user_name": "yangwei",
-        "pwd": "jianxun123"
+        "pwd": "xxxxxxx123"
     },
     "8": {
         "ip": "192.168.0.8",
         "user_name": "yangwei",
-        "pwd": "jianxun123"
+        "pwd": "xxxxxxx123"
     },
     "9": {
         "ip": "192.168.0.9",
         "user_name": "yangwei",
-        "pwd": "jianxun123"
+        "pwd": "xxxxxxx123"
     },
     "10": {
         "ip": "192.168.0.10",
         "user_name": "yangwei",
-        "pwd": "jianxun123"
+        "pwd": "xxxxxxx123"
     },
     "22": {
         "ip": "192.168.0.22",
         "user_name": "yangwei",
-        "pwd": "jianxun123"
+        "pwd": "xxxxxxx123"
     }
 }
 
@@ -199,31 +199,31 @@ TenXunYun_jobs = {
     "18": {
         "ip": "10.0.0.18",
         "user_name": "yangwei",
-        "pwd": "jianxun123"
+        "pwd": "xxxxxxx123"
     },
     "25": {
         "ip": "10.0.0.25",
         "user_name": "yangwei",
-        "pwd": "jianxun123"
+        "pwd": "xxxxxxx123"
     },
     "26": {
         "ip": "10.0.0.26",
         "user_name": "yangwei",
-        "pwd": "jianxun123"
+        "pwd": "xxxxxxx123"
     },
     "36": {
         "ip": "10.0.0.36",
         "user_name": "yangwei",
-        "pwd": "jianxun123"
+        "pwd": "xxxxxxx123"
     },
     "39": {
         "ip": "10.0.0.39",
         "user_name": "yangwei",
-        "pwd": "jianxun123"
+        "pwd": "xxxxxxx123"
     },
     "48": {
         "ip": "10.0.0.48",
         "user_name": "yangwei",
-        "pwd": "jianxun123"
+        "pwd": "xxxxxxx123"
     }
 }
